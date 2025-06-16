a727a42b6d3ce9e97d0385cb6e76b055b4ef3237
constf03 <stefano.confalone03@gmail.com>
2025-06-16 15:13:40 +0300

    fix navigation

--- public/scripts/navigation.js
+++ public/scripts/navigation.js
@@ -1,5 +1,13 @@
 const startBtn = document.getElementById("btn-start");
+if (startBtn != null) {
+  startBtn.addEventListener("click", () => {
+    window.location.href = "./screen_start.html";
+  });
+}
 
-startBtn.addEventListener("click", () => {
-  window.location.href = "./screen_start.html";
-});
+const back = document.getElementById("btn-nav-back");
+if (back != null) {
+  back.addEventListener("click", () => {
+    history.back(-1);
+  });
+}
