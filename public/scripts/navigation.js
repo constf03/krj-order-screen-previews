d480d09ab5383904f4b6e006c15c5f3e40665bad
constf03 <stefano.confalone03@gmail.com>
2025-06-18 14:50:58 +0300

    update navigation

--- public/scripts/navigation.js
+++ public/scripts/navigation.js
@@ -1,3 +1,5 @@
+const path = window.location.pathname;
+
 const startBtn = document.getElementById("btn-start");
 if (startBtn != null) {
   startBtn.addEventListener("click", () => {
@@ -5,9 +7,25 @@ if (startBtn != null) {
   });
 }
 
+const next = document.getElementById("btn-nav-next");
+if (next != null) {
+  next.addEventListener("click", () => {
+    switch (path) {
+      case "/public/screen_start.html":
+        window.location.href = "./screen_order_main.html";
+        break;
+      default:
+        break;
+    }
+  });
+}
+
 const back = document.getElementById("btn-nav-back");
 if (back != null) {
   back.addEventListener("click", () => {
+    if (path == "/public/screen_start.html") {
+      localStorage.setItem("whereToEatOption", undefined);
+    }
     history.back(-1);
   });
 }
